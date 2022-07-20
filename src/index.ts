4586979cff66fb0125eaa77eda27336367708499
Roberto Cortés Monroy <robertocortesmonroy@Robertos-MacBook-Air.local>
2022-07-20 11:21:22 -0500

    [add] init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express from "express";
 import path from "path";
 import diaryRouter from "./routes/mailsender";
+import cors from "cors";
+
+const allowedOrigins = ["https://whoisbeto.dev"];
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " + "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = 3001;
@@ -12,6 +28,7 @@ app.get("/ping", (_, res): void => {
 });
 
 app.use("/api/send", diaryRouter);
+app.use(cors());
 
 app.listen(port, (): void => {
   console.log("APP IS RUNNING IN PORT", port);
--- src/routes/mailsender.ts
+++ src/routes/mailsender.ts
@@ -26,7 +26,7 @@ router.post("/", async (req, res): Promise<void> => {
       <meta name="x-apple-disable-message-reformatting">
       <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
       <title></title>
-      
+
         <style type="text/css">
           @media only screen and (min-width: 620px) {
       .u-row {
@@ -35,13 +35,13 @@ router.post("/", async (req, res): Promise<void> => {
       .u-row .u-col {
         vertical-align: top;
       }
-    
+
       .u-row .u-col-100 {
         width: 600px !important;
       }
-    
+
     }
-    
+
     @media (max-width: 620px) {
       .u-row-container {
         max-width: 100% !important;
@@ -67,41 +67,39 @@ router.post("/", async (req, res): Promise<void> => {
       margin: 0;
       padding: 0;
     }
-    
+
     table,
     tr,
     td {
       vertical-align: top;
       border-collapse: collapse;
     }
-    
+
     p {
       margin: 0;
     }
-    
+
     .ie-container table,
     .mso-container table {
       table-layout: fixed;
     }
-    
+
     * {
       line-height: inherit;
     }
-    
+
     a[x-apple-data-detectors='true'] {
       color: inherit !important;
       text-decoration: none !important;
     }
-    
+
     table, td { color: #000000; } a { color: #0000ee; text-decoration: underline; } @media (max-width: 480px) { #u_column_1 .v-col-padding { padding: 7px 0px 5px !important; } #u_content_heading_1 .v-font-size { font-size: 36px !important; } #u_content_image_4 .v-container-padding-padding { padding: 13px 70px 4px 10px !important; } #u_content_image_4 .v-src-width { width: auto !important; } #u_content_image_4 .v-src-max-width { max-width: 68% !important; } }
         </style>
-      
-      
-    
+
     <!--[if !mso]><!--><link href="https://fonts.googleapis.com/css?family=Cabin:400,700" rel="stylesheet" type="text/css"><!--<![endif]-->
-    
+
     </head>
-    
+
     <body class="clean-body u_body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #eccafa;color: #000000">
       <!--[if IE]><div class="ie-container"><![endif]-->
       <!--[if mso]><div class="mso-container"><![endif]-->
@@ -110,71 +108,70 @@ router.post("/", async (req, res): Promise<void> => {
       <tr style="vertical-align: top">
         <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
         <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #eccafa;"><![endif]-->
-        
-    
+
     <div class="u-row-container" style="padding: 0px;background-image: url('https://cdn.templates.unlayer.com/assets/1652249845134-1652094644660-bg.jpg');background-repeat: no-repeat;background-position: center top;background-color: transparent">
       <div class="u-row" style="Margin: 0 auto;min-width: 320px;max-width: 600px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
         <div style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
           <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-image: url('https://cdn.templates.unlayer.com/assets/1652249845134-1652094644660-bg.jpg');background-repeat: no-repeat;background-position: center top;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: transparent;"><![endif]-->
-          
+
     <!--[if (mso)|(IE)]><td align="center" width="600" class="v-col-padding" style="width: 600px;padding: 50px 0px 54px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;" valign="top"><![endif]-->
     <div id="u_column_1" class="u-col u-col-100" style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
       <div style="height: 100%;width: 100% !important;">
       <!--[if (!mso)&(!IE)]><!--><div class="v-col-padding" style="padding: 50px 0px 54px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;"><!--<![endif]-->
-      
+
     <table id="u_content_heading_1" style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
       <h1 class="v-font-size" style="margin: 0px; color: #ffffff; line-height: 110%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: 'Cabin',sans-serif; font-size: 45px;">
         <strong>Thank you </strong><br /><strong>for your collaboration</strong>
       </h1>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
     <table id="u_content_image_4" style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:10px 91px 10px 10px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
     <table width="100%" cellpadding="0" cellspacing="0" border="0">
       <tr>
         <td style="padding-right: 0px;padding-left: 0px;" align="right">
-          
+
           <img align="right" border="0" src="https://cdn.templates.unlayer.com/assets/1652183201329-img.png" alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 62%;max-width: 309.38px;" width="309.38" class="v-src-width v-src-max-width"/>
-          
+
         </td>
       </tr>
     </table>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
     <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:60px 10px 20px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
       <h1 class="v-font-size" style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: 'Cabin',sans-serif; font-size: 22px;">
         <strong>Dear whoIsBeto</strong>
       </h1>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
     <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:0px 45px 10px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
       <div style="line-height: 170%; text-align: center; word-wrap: break-word;">
         <p style="font-size: 14px; line-height: 170%; text-align: center;">${message}</p>
       </div>
@@ -182,45 +179,44 @@ router.post("/", async (req, res): Promise<void> => {
         </tr>
       </tbody>
     </table>
-    
+
     <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:3px 10px 0px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
       <h1 class="v-font-size" style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: 'Cabin',sans-serif; font-size: 17px;">
         Best Regards
       </h1>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
     <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:0px 10px 182px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
       <h1 class="v-font-size" style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: 'Cabin',sans-serif; font-size: 24px;">
         -${name}- (${email})
       </h1>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
     <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
       <tbody>
         <tr>
           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:14px;font-family:arial,helvetica,sans-serif;" align="left">
-            
+
     <div align="center">
       <div style="display: table; max-width:207px;">
       <!--[if (mso)|(IE)]><table width="207" cellpadding="0" cellspacing="0" border="0"><tr><td style="border-collapse:collapse;" align="center"><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse; mso-table-lspace: 0pt;mso-table-rspace: 0pt; width:207px;"><tr><![endif]-->
-      
-        
+
         <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 20px;" valign="top"><![endif]-->
         <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 20px">
           <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
@@ -230,7 +226,7 @@ router.post("/", async (req, res): Promise<void> => {
           </td></tr>
         </tbody></table>
         <!--[if (mso)|(IE)]></td><![endif]-->
-        
+
         <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 20px;" valign="top"><![endif]-->
         <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 20px">
           <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
@@ -240,7 +236,7 @@ router.post("/", async (req, res): Promise<void> => {
           </td></tr>
         </tbody></table>
         <!--[if (mso)|(IE)]></td><![endif]-->
-        
+
         <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 20px;" valign="top"><![endif]-->
         <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 20px">
           <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
@@ -250,7 +246,7 @@ router.post("/", async (req, res): Promise<void> => {
           </td></tr>
         </tbody></table>
         <!--[if (mso)|(IE)]></td><![endif]-->
-        
+
         <!--[if (mso)|(IE)]><td width="32" style="width:32px; padding-right: 0px;" valign="top"><![endif]-->
         <table align="left" border="0" cellspacing="0" cellpadding="0" width="32" height="32" style="width: 32px !important;height: 32px !important;display: inline-block;border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;margin-right: 0px">
           <tbody><tr style="vertical-align: top"><td align="left" valign="middle" style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
@@ -260,17 +256,16 @@ router.post("/", async (req, res): Promise<void> => {
           </td></tr>
         </tbody></table>
         <!--[if (mso)|(IE)]></td><![endif]-->
-        
-        
+
         <!--[if (mso)|(IE)]></tr></table></td></tr></table><![endif]-->
       </div>
     </div>
-    
+
           </td>
         </tr>
       </tbody>
     </table>
-    
+
       <!--[if (!mso)&(!IE)]><!--></div><!--<![endif]-->
       </div>
     </div>
@@ -279,8 +274,7 @@ router.post("/", async (req, res): Promise<void> => {
         </div>
       </div>
     </div>
-    
-    
+
         <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
         </td>
       </tr>
@@ -289,7 +283,7 @@ router.post("/", async (req, res): Promise<void> => {
       <!--[if mso]></div><![endif]-->
       <!--[if IE]></div><![endif]-->
     </body>
-    
+
     </html>
 
     ` // html body
