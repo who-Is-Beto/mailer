d70b6d931598d3e3e7f4f1396d01e939dedab8f1
Roberto Cortés Monroy <robertocortesmonroy@Robertos-MacBook-Air.local>
2022-08-12 18:17:30 -0500

    [add] public v1.1.2

--- src/envs.ts
+++ src/envs.ts
@@ -3,6 +3,7 @@ const whoisbetoVercel = process.env.VERCEL_URL;
 
 export const environment = {
   aviableDomains: [whoisbeto, whoisbetoVercel] as string[],
+  localhost: process.env.LOCAL_DEVELOPMENT,
   port: process.env.PORT || 3000,
   whoIsBetoMail: process.env.WHO_IS_BETO_MAIL,
   mailHost: process.env.MAIL_HOST,
