f2010ddbc483391ecb8fb8f589a767bd54ba95a2
Roberto Cortés Monroy <robertocortesmonroy@Robertos-MacBook-Air.local>
2022-08-15 22:49:28 -0500

    [add] public v1.1.2

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,7 @@ import { environment } from "./envs";
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (environment.aviableDomains.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " + "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = environment.port;
