79c58522e5e4b42f1b9a21831373273b8ee29b45
Roberto Cortés Monroy <robertocortesmonroy@Robertos-MacBook-Air.local>
2022-08-29 00:02:33 -0500

    [add] public v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,12 @@ import express from "express";
 import "dotenv/config";
 import path from "path";
 import diaryRouter from "./routes/mailsender";
-import cors from "cors";
 import { environment } from "./envs";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: environment.aviableDomains.map((domain) => domain)
-  })
-);
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = environment.port;
@@ -20,7 +16,6 @@ app.get("/ping", (_, res): void => {
 });
 
 app.use("/api/send", diaryRouter);
-app.use(cors());
 
 app.listen(port, (): void => {
   console.log("APP IS RUNNING IN PORT", port);
